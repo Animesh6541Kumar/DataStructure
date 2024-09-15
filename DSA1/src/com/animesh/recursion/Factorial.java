c1eecfe59d613219f0eb4aa2d030175337405a84
Animesh6541Kumar <animesh316541@gmail.com>
2024-09-16 00:10:09 +0530

    Factorial Program

--- DSA1/src/com/animesh/recursion/Factorial.java
+++ DSA1/src/com/animesh/recursion/Factorial.java
@@ -2,8 +2,18 @@ package com.animesh.recursion;
 
 public class Factorial {
 
+	public static int factorial(int n) {
+		if(n==0 || n==1) {
+			return 1;
+		}else {
+			return n*factorial(n-1);
+		}
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int value =factorial(6);
+		System.out.println("Factioral of 6 -:"+value);
 
 	}
 
